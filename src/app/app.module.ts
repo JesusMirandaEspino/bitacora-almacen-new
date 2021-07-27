d28608a43f03f4b2fd7950ab6e34da427419684f
JesusMirandaEspino <jesus.voluntadheredada.human@gmail.com>
2021-07-27 16:50:22 -0500

    pages component error

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,10 +6,16 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { IngresarComponent } from './ingresar/ingresar.component';
 import { FooterComponent } from './footer/footer.component';
+import { PagesModule } from './pages/pages.module';
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HeaderComponent, IngresarComponent, FooterComponent ],
-  bootstrap:    [ AppComponent ]
+  imports: [BrowserModule, FormsModule, PagesModule],
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    IngresarComponent,
+    FooterComponent
+  ],
+  bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
