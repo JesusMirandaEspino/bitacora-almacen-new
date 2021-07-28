6a2603fc3567f6b9239a5e9e6398fb4848921024
JesusMirandaEspino <jesus.voluntadheredada.human@gmail.com>
2021-07-28 17:01:35 -0500

    new class asigned

--- src/app/models/atenciontiendas.class.ts
+++ src/app/models/atenciontiendas.class.ts
@@ -1,13 +1,38 @@
 //Clase principal del llenado de informacion de la parte de Atencion a tiendas
 export class atencionTiendas {
   fecha: Date;
+  fechaEntraga: Date;
   nombreCliente: String;
   domicilio: String;
   Municipio: String;
+  estado: EstadoPedido;
+  tipo: TipoPedido;
+  domiclio: Domicilio;
 
   //  tienda origen sera un objeto con una lista de tiendas que seran consumidas de una base de datos, ingresadas y modificadas por un administrador
   tiendaOrigen: String;
   recibio: String;
   piezasSurtidas: Number;
   piezasrequeridas: Number;
+  telefono: String;
+  notas: String;
+}
+
+class Domicilio {
+  colonia: String;
+  municipio: String;
+}
+
+class EstadoPedido {
+  entregado: String;
+  pendiente: String;
+  porEntregar: String;
+  Surtido: String;
+}
+
+class TipoPedido {
+  domicilio: String;
+  paqueteria: String;
+  almacen: String;
+  recogeTienda: String;
 }
---
+++ src/app/models/jefeAlmacen.ts
@@ -0,0 +1,24 @@
+// Clase para la informacion que estara llenando el jefe de almacen
+class JefeAlmacen {
+  ronda: Number;
+
+  // Este es asignado preliminarmente por un administrador
+
+  Chofer: String;
+  // Este es asignado preliminarmente por un administrador
+  copiloto: String;
+
+  horaSalida: Date;
+  horaLlegada: Date;
+  horaReferencia: String;
+
+  // Este es asignado preliminarmente por un administrador
+  unidad: String;
+
+  kmSalida: String;
+  kmEntrada: String;
+
+  // El tipo de incidencia esta asignado de manera preliminar
+  incidencia: String;
+  anotaciones: String;
+}
